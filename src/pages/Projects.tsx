7dd6a14ce1c8bc9f723451d8cb0c222858f74347
lock-desk <pintepinturasdeveloper@gmail>
2025-08-13 15:39:49 -0300

    feat: add fourth column to projects grid on extra-large screens

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -203,7 +203,7 @@ const Projects = () => {
         </div>
 
         {/* Projects Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
           {allProjects.map((item, idx) => (
             <div
               key={idx}
